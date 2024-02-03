f59a3f0df1bd7b8fc38c500079e02fea9565f52a
Restyled.io <commits@restyled.io>
2024-02-03 18:50:57 +0000

    Restyled by clang-format

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,35 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import {resolve} from "path";
+import {defineConfig} from "vite";
 import banner from 'vite-plugin-banner';
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import {nodePolyfills} from 'vite-plugin-node-polyfills'
 import removeConsole from "vite-plugin-remove-console";
+
 import pkg from './package.json';
 
 /// <reference types="vitest" />
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  test: {
-		reporters: ["html", "default"],
-     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-       reportsDirectory: './tests-c/unit/coverage'
+  test : {
+    reporters : [ "html", "default" ],
+    coverage : {
+      provider : 'v8',
+      reporter : [ 'text', 'json', 'html' ],
+      reportsDirectory : './tests-c/unit/coverage'
     },
-	},
-   plugins: [
-     nodePolyfills(),
-     removeConsole(),
-    banner(
-      `/**\n * name: ${pkg.name}\n * version: v${pkg.version}\n * description: ${pkg.description}\n * author: ${pkg.author}\n * homepage: ${pkg.homepage}\n */`
-    ),
+  },
+  plugins : [
+    nodePolyfills(),
+    removeConsole(),
+    banner(`/**\n * name: ${pkg.name}\n * version: v${
+        pkg.version}\n * description: ${pkg.description}\n * author: ${
+        pkg.author}\n * homepage: ${pkg.homepage}\n */`),
   ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/lib/main.ts"),
-      name: "Emoxy",
-      fileName: "emoxy",
+  build : {
+    lib : {
+      entry : resolve(__dirname, "src/lib/main.ts"),
+      name : "Emoxy",
+      fileName : "emoxy",
     },
-   
+
   },
 });
