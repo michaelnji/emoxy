9e7f794153395ce5794d1a7ec1d47e93fcb74c27
COD3VIL <codebase65@outlook.com>
2024-02-06 22:43:18 +0100

    docs: 📝 added some documentation using jsDoc comments

--- src/lib/initiator/index.ts
+++ src/lib/initiator/index.ts
@@ -1,4 +1,3 @@
-import ky from "ky";
 import {
 	EmoxyEmoji,
 	EmoxyEmojiCategory,
@@ -12,26 +11,82 @@ import {
  * @class Emoxy
  */
 export class Emoxy {
-	// base emojihub API url
+	/**
+	 * @description Emojihub api URL
+	 * @private
+	 * @memberof Emoxy
+	 */
 	private BASE_URL = "https://emojihub.yurace.pro/api";
 
 	/**
-	 * @description gets all emojis
-	 * @return {*}  {(Promise<{
-	 * 		code: number;
-	 * 		message: string;
-	 * 		emojis: EmoxyEmoji[] | unknown;
-	 * 	}>)}
+	 * @description Gets all emojis
+	 * @return {*}  {Promise<EmoxyResponseGroup>}
 	 * @memberof Emoxy
 	 */
 	async all_emojis(): Promise<EmoxyResponseGroup> {
 		const url = `${this.BASE_URL}/all`;
 		try {
-			const response: EmoxyEmoji[] = await ky.get(url).json();
+			const response = await fetch(url);
+			const data = await response.json();
+			const emojis: EmoxyEmoji[] = data;
 			return {
 				code: 200,
 				message: "Success",
-				emojis: response,
+				emojis,
+			};
+		} catch (error) {
+			return {
+				code: 500,
+				message: `${error}`,
+				emojis: null,
+			};
+		}
+	}
+
+	/**
+	 * @description Returns a random emoji from a given category
+	 * @param {EmoxyEmojiCategory} category
+	 * @return {*}  {Promise<EmoxyResponse>}
+	 * @memberof Emoxy
+	 */
+	async random_emoji_cat(category: EmoxyEmojiCategory): Promise<EmoxyResponse> {
+		const url = `${this.BASE_URL}/random/category/${category}`;
+		try {
+			const response = await fetch(url);
+			const data = await response.json();
+			const emoji: EmoxyEmoji = data;
+			return {
+				code: 200,
+				message: "Success",
+				emoji,
+			};
+		} catch (error) {
+			return {
+				code: 500,
+				message: "Fetch Error: failed to fetch",
+				emoji: null,
+			};
+		}
+	}
+
+	/**
+	 * @description Gets all emojis from a given category
+	 * @param {EmoxyEmojiCategory} category
+	 * @return {*}  {Promise<EmoxyResponseGroup>}
+	 * @memberof Emoxy
+	 */
+	async all_emoji_cat(
+		category: EmoxyEmojiCategory,
+	): Promise<EmoxyResponseGroup> {
+		const url = `${this.BASE_URL}/all/category/${category}`;
+		try {
+			const response = await fetch(url);
+			const data = await response.json();
+			const emojis: EmoxyEmoji[] = data;
+			return {
+				code: 200,
+				message: "Success",
+				emojis,
 			};
 		} catch (error) {
 			return {
@@ -42,50 +97,22 @@ export class Emoxy {
 		}
 	}
 
-	async random_emoji_cat(category: EmoxyEmojiCategory): Promise<EmoxyResponse> {
-		const url = `${this.BASE_URL}/random/category/${category}`;
-		try {
-			const response: EmoxyEmoji = await ky.get(url).json();
-			return {
-				code: 200,
-				message: "Success",
-				emoji: response,
-			};
-		} catch (error) {
-			return {
-				code: 500,
-				message: "Fetch Error: failed to fetch",
-				emoji: null,
-			};
-		}
-	}
-	async all_emoji_cat(
-		category: EmoxyEmojiCategory,
-	): Promise<EmoxyResponseGroup> {
-		const url = `${this.BASE_URL}/all/category/${category}`;
-		try {
-			const response: EmoxyEmoji[] = await ky.get(url).json();
-			return {
-				code: 200,
-				message: "Success",
-				emojis: response,
-			};
-		} catch (error) {
-			return {
-				code: 500,
-				message: "Fetch Error: failed to fetch",
-				emojis: null,
-			};
-		}
-	}
+	/**
+	 * @description Gets a random emoji from a given group
+	 * @param {string} group
+	 * @return {*}  {Promise<EmoxyResponse>}
+	 * @memberof Emoxy
+	 */
 	async random_emoji_group(group: string): Promise<EmoxyResponse> {
 		const url = `${this.BASE_URL}/random/group/${group}`;
 		try {
-			const response: EmoxyEmoji = await ky.get(url).json();
+			const response = await fetch(url);
+			const data = await response.json();
+			const emoji: EmoxyEmoji = data;
 			return {
 				code: 200,
 				message: "Success",
-				emoji: response,
+				emoji,
 			};
 		} catch (error) {
 			return {
@@ -95,14 +122,23 @@ export class Emoxy {
 			};
 		}
 	}
+
+	/**
+	 * @description Gets all emojis from a given group
+	 * @param {string} group
+	 * @return {*}  {Promise<EmoxyResponseGroup>}
+	 * @memberof Emoxy
+	 */
 	async all_emoji_group(group: string): Promise<EmoxyResponseGroup> {
 		const url = `${this.BASE_URL}/all/group/${group}`;
 		try {
-			const response: EmoxyEmoji[] = await ky.get(url).json();
+			const response = await fetch(url);
+			const data = await response.json();
+			const emojis: EmoxyEmoji[] = data;
 			return {
 				code: 200,
 				message: "Success",
-				emojis: response,
+				emojis,
 			};
 		} catch (error) {
 			return {
@@ -113,3 +149,5 @@ export class Emoxy {
 		}
 	}
 }
+
+
