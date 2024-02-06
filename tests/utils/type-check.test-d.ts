71ce5d16e70c8aa4722b973706a68c998975c055
COD3VIL <codebase65@outlook.com>
2024-02-07 00:34:23 +0100

    fix: 🐛 rearrange folder structure and merge all declaration files

--- tests/utils/type-check.test-d.ts
+++ tests/utils/type-check.test-d.ts
@@ -1,5 +1,5 @@
 import { expectTypeOf, test } from "vitest";
-import { getSpecificGroups } from "../../src/lib/main";
+import { getSpecificGroups } from "../../src";
 
 test("Types work properly", () => {
 	expectTypeOf(getSpecificGroups).parameter(0).toBeString();
